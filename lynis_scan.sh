8df2d2b45de84cbb8857ad8aaa107a6f8ddbde32
Faris AL-Otaibi <76238196+farisc0de@users.noreply.github.com>
2024-06-19 06:29:18 +0300

    Update lynis_scan.sh

--- lynis_scan.sh
+++ lynis_scan.sh
@@ -3,25 +3,25 @@
 source $1
 
 function lynis_scan {
-
+    
     # Check if lynis is installed
-
+    
     if ! command -v lynis &> /dev/null; then
         changed=false
         msg="Lynis is not installed"
         return
     fi
-
+    
     # Check if the user is root
-
+    
     if [ "$EUID" -ne 0 ]; then
         changed=false
         msg="Please run this script as root"
         return
     fi
-
+    
     lynis audit system --auditor "Ansible" > /tmp/lynis_scan.log
-
+    
     if [ $? -eq 0 ]; then
         changed=true
         msg="Lynis scan completed successfully"
@@ -34,46 +34,45 @@ function lynis_scan {
 
 function lynis_scan_schedule {
     
-        # Check if lynis is installed
+    # Check if lynis is installed
     
-        if ! command -v lynis &> /dev/null; then
-            changed=false
-            msg="Lynis is not installed"
-            return
-        fi
+    if ! command -v lynis &> /dev/null; then
+        changed=false
+        msg="Lynis is not installed"
+        return
+    fi
     
-        # Check if the user is root
+    # Check if the user is root
     
-        if [ "$EUID" -ne 0 ]; then
-            changed=false
-            msg="Please run this script as root"
-            return
-        fi
+    if [ "$EUID" -ne 0 ]; then
+        changed=false
+        msg="Please run this script as root"
+        return
+    fi
     
-        lynis audit system --cronjob --quiet --auditor "Ansible"
+    lynis audit system --cronjob --quiet --auditor "Ansible"
     
-        if [ $? -eq 0 ]; then
-            changed=true
-            msg="Lynis scan scheduled successfully"
-        else
-            changed=false
-            msg="Lynis scan scheduling failed"
-        fi
-    
+    if [ $? -eq 0 ]; then
+        changed=true
+        msg="Lynis scan scheduled successfully"
+    else
+        changed=false
+        msg="Lynis scan scheduling failed"
+    fi
 }
 
 
 case $state in
     scan)
         lynis_scan
-        ;;
+    ;;
     schedule)
         lynis_scan_schedule
-        ;;
+    ;;
     *)
         changed=false
         msg="Invalid state"
-        ;;
+    ;;
 esac
 
 printf '{"changed": %s, "msg": "%s", "contents": %s}' "$changed" "$msg" "$contents"
